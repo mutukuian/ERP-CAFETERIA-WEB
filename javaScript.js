8dedc63c71a5f18c31086611af3e64048dd87f09
GathirwaNicholus <nicholusgathirwa04@gmail.com>
2023-11-24 12:27:36 +0300

    Implemented menu for the cafeteria

---
+++ javaScript.js
@@ -0,0 +1,12 @@
+function submit()
+{
+    var var1 = document.getElementById("quantity1");
+    var var2 = document.getElementById("coffee");
+
+    var some = document.getElementById(coffee1);
+    some.innerHTML = 20;
+
+    var var3 = document.getElementById("quantity2");
+    var var4 = document.getElementById("cupcake");
+}
+console.log("HEllo");
